ae351260150d7170fdf8dfa2ea502b9344bcf385
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-05-11 08:50:13 +0000

    Create views for account linking process
    Views based on those used in account retrieval process

---
+++ htdocs/web_portal/views/user/link_account_accepted.php
@@ -0,0 +1,6 @@
+<div class="rightPageContainer">
+    <h1 class="Success">Success</h1>
+    Your request to link another account has been accepted. An e-mail has
+    been sent to the address registered with this account. Please follow the
+    instructions contained within it to complete your account linking. <br />
+</div>
---
+++ htdocs/web_portal/views/user/linked_account.php
@@ -0,0 +1,4 @@
+<div class="rightPageContainer">
+    <h1 class="Success">Success</h1>
+    Your account has been successfully linked<br />
+</div>
