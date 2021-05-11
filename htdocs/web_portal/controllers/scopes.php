56c976bc5f6dd6e4a407dd2d6355a1e024a504a7
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-05-11 11:16:35 +0000

    Pass parameter based on if user is an admin
    Rather than throwing an exception when the user is not an admin, instead set a parameter which will be used to toggle admin permissions.

--- htdocs/web_portal/controllers/scopes.php
+++ htdocs/web_portal/controllers/scopes.php
@@ -3,18 +3,18 @@ require_once __DIR__.'/utils.php';
 require_once __DIR__ . '/../../web_portal/components/Get_User_Principle.php';
 
 function show_scopes(){
-    //Check the user has permission to see the page, will throw exception
-    //if correct permissions are lacking
-    checkUserIsAdmin();
 
     $scopes = \Factory::getScopeService()->getScopes();
-    $params['Scopes']= $scopes;
+    $params['Scopes'] = $scopes;
 
-
-
-    $dn = Get_User_Principle();
-    $user = \Factory::getUserService()->getUserByPrinciple($dn);
+    $idString = Get_User_Principle();
+    $user = \Factory::getUserService()->getUserByPrinciple($idString);
     $params['portalIsReadOnly'] = portalIsReadOnlyAndUserIsNotAdmin($user);
+
+    $params['UserIsAdmin'] = false;
+    if(!is_null($user)) {
+        $params['UserIsAdmin'] = $user->isAdmin();
+    }
 
     show_view('scopes.php', $params, 'Scopes');
     die();
