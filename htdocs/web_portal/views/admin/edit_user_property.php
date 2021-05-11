e775281950ecce44e05429ca3e01b535258a3b84
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-05-11 16:15:10 +0000

    Update references to id strings from dns

--- htdocs/web_portal/views/admin/edit_user_property.php
+++ htdocs/web_portal/views/admin/edit_user_property.php
@@ -1,8 +1,8 @@
 <div class="rightPageContainer">
-    <h1>Update Certificate DN</h1>
+    <h1>Update User Property</h1>
     <br />
     <br />
-    The current certificate DN for
+    The ID string for
     <b><?php echo xssafe($params['Title']) ." ". xssafe($params['Forename']) ." ". xssafe($params['Surname']) ?></b>
     is:
     <br />
@@ -11,7 +11,7 @@
     <br />
     <form class="inputForm" method="post" action="index.php?Page_Type=Admin_Edit_User_Property" name="editSType">
         <span class="input_name">New Certificate DN</span>
-        <input type="text" value="<?php xecho($params['CertDN']) ?>" name="DN" class="input_input_text">
+        <input type="text" value="<?php xecho($params['CertDN']) ?>" name="IDSTRING" class="input_input_text">
         <input class="input_input_hidden" type="hidden" name="ID" value="<?php echo $params['ID'] ?>" />
         <br />
         <input type="submit" value="Update DN" class="input_button">
