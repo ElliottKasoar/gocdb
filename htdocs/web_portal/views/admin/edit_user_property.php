dbe0b04783db129d0c7b4e323c225f766913b6d4
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-05-12 09:29:19 +0000

    Update ID string input name

--- htdocs/web_portal/views/admin/edit_user_property.php
+++ htdocs/web_portal/views/admin/edit_user_property.php
@@ -11,7 +11,7 @@
     <br />
     <form class="inputForm" method="post" action="index.php?Page_Type=Admin_Edit_User_Property" name="editSType">
         <span class="input_name">New ID String</span>
-        <input type="text" value="<?php xecho($params['IdString']) ?>" name="IDSTRING" class="input_input_text">
+        <input type="text" value="<?php xecho($params['IdString']) ?>" name="IdString" class="input_input_text">
         <input class="input_input_hidden" type="hidden" name="ID" value="<?php echo $params['ID'] ?>" />
         <br />
         <input type="submit" value="Update ID String" class="input_button">
