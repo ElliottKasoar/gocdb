958485e939bd6b55cd59b7c73ee7ece7c2c9e7a6
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-06-02 16:12:55 +0000

    Order auth types based on MyConfig1
    Rather than hardcoding the entire auth types list, only the auth types for each token type must be hardcoded now. If the order of tokens is changed via MyConfig1, this will be reflected in the order of the authe types. In the future the auth realms may be extracted automatically too. Also added a flag to only return the "main" in-use auth types i.e. only EGI for Shibboleth, as this sublist is what will be used for API outputs and linking, but in general the other types should be valid.

--- lib/Gocdb_Services/User.php
+++ lib/Gocdb_Services/User.php
@@ -469,11 +469,36 @@ class User extends AbstractEntityService{
 
     /**
      * Returns list of authentication types
-     * Todo: explicitly link to supported auth methods in MyConfig1
-     * @return array of types
+     * List composed of AuthenticationRealms defined within tokens
+     * Order of tokens determined by order listed in MyConfig1
+     * Order of realms hardcoded based on order within tokens
+     * @param bool $reducedRealms if true only return the "main" authentication types
+     * @return array of authentication types
      */
-    public function getAuthTypes() {
-        $authTypes = ['IGTF X509 Cert', 'EGI', 'IRIS IAM - OIDC', 'FAKE'];
+    public function getAuthTypes($reducedRealms=true) {
+
+        // Get list of tokens in order they are currently used
+        $myConfig1 = new \org\gocdb\security\authentication\MyConfig1();
+        $authTokenNames = $myConfig1->getAuthTokenClassList();
+
+        // Hardcoded auth realms in same order as in token definitions
+        $x509Realms = ['IGTF X509 Cert'];
+        if ($reducedRealms) {
+            $shibRealms = ['EGI Proxy IdP'];
+        } else {
+            $shibRealms = ['EUDAT_SSO_IDP', 'UK_ACCESS_FED', 'EGI Proxy IdP'];
+        }
+
+        // Add auth types to a list in the correct order
+        $authTypes = array();
+        foreach ($authTokenNames as $authTokenName) {
+            if (strpos($authTokenName, 'Shib') !== false) {
+                $authTypes = array_merge($authTypes, $shibRealms);
+            }
+            if (strpos($authTokenName, 'X509') !== false) {
+                $authTypes = array_merge($authTypes, $x509Realms);
+            }
+        }
         return $authTypes;
     }
 
