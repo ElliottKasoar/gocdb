ac56870a30bd6a7be7bb733d00ea72051e2bc5ff
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2021-06-24 17:08:33 +0100

    Update run_tests.sh

--- .actions/run_tests.sh
+++ .actions/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "${{ github.workspace }}" ]]; then
+if [[ true ]]; then
     
     # Check modified PHP files with PHP's internal syntax checker
     git diff --name-only --diff-filter=ACMRTUXB HEAD^ | grep '\.php$' | xargs -r -n 1 php -l || exit 1
