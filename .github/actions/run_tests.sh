b46e9078a0ff655dba761e102a97b14852a70df6
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2021-07-01 16:36:28 +0100

    Remove echo

--- .github/actions/run_tests.sh
+++ .github/actions/run_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "ACTIONS VAR: $GITHUB_ACTIONS"
-
 if [[ $GITHUB_ACTIONS ]]; then
     # Check modified PHP files with PHP's internal syntax checker
     git diff --name-only --diff-filter=ACMRTUXB HEAD^ | grep '\.php$' | xargs -r -n 1 php -l || exit 1
