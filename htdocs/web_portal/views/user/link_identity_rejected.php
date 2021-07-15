21fa76d9bf504731511f7bcae0bc852f0cb5cfa9
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-07-15 09:58:49 +0000

    Create view to explain why users cannot link

---
+++ htdocs/web_portal/views/user/link_identity_rejected.php
@@ -0,0 +1,19 @@
+<div class="rightPageContainer">
+    <h1 class="Error">Error</h1>
+    <p>You cannot recover or link  your identifier to another account while registered with an account
+    associated with multiple identifiers. Your current identifier is:</p>
+    
+    <?php echo "<p>{$params['CURRENTAUTHTYPE']}: {$params['IDSTRING']}</p>"?>
+    
+    <p>The other identifiers associated with this account are:</p>
+    <ul>
+        <?php foreach ($params['OTHERPROPERTIES'] as $prop) {
+            echo "<li> {$prop->getKeyName()}: {$prop->getKeyValue()} </li>";
+        } ?>
+    </ul>
+
+    <p>If you wish to associate your current identifier with another account,
+    please unlink all other identifiers first.</p>
+    <p>If you wish to add new identifiers to this account, please
+    access GOCDB while authenticated with the new identifer.</p>
+</div>
