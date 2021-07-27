b363048d8d92f81f85d172d6eb1465a4c28679bb
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-07-27 16:18:37 +0000

    Delete link requests older than one day

--- resources/DeleteOldRequestsRunner.php
+++ resources/DeleteOldRequestsRunner.php
@@ -4,30 +4,30 @@ require_once dirname(__FILE__) . "/../lib/Doctrine/bootstrap.php";
 require dirname(__FILE__) . '/../lib/Doctrine/bootstrap_doctrine.php';
 require_once dirname(__FILE__) . '/../lib/Gocdb_Services/Factory.php';
 
+// Use UTC (set elsewhere by index.php)
+date_default_timezone_set("UTC");
+
 echo "Querying identity linking and account recovery requests\n\n";
 
 $em = $entityManager;
 $dql = "SELECT l FROM LinkIdentityRequest l";
 $requests = $entityManager->createQuery($dql)->getResult();
 
+// Remove requests older than one day
 $nowUtc = new \DateTime(null, new \DateTimeZone('UTC'));
+$oneDay = \DateInterval::createFromDateString('1 days');
+$yesterdayUtc = $nowUtc->sub($oneDay);
 
 echo "Starting scan of request creation dates at: " . $nowUtc->format('D, d M Y H:i:s') . "\n\n";
+
 foreach ($requests as $request) {
    
     $creationDate = $request->getCreationDate()->setTimezone(new \DateTimeZone('UTC'));
 
-    // Will want one day? Currently 30 min
-    $oneDay = \DateInterval::createFromDateString('30 minutes');
-    $yesterdayUtc = $nowUtc->sub($oneDay);
-
     if ($yesterdayUtc > $creationDate) {
-        echo "yesterdayUtc is greater than creationDate\n";
-        echo "Yesterday time: " . $yesterdayUtc->format('D, d M Y H:i:s') . "\n";
-        echo "Request " . $request->getId() . " (creation date: " . $creationDate->format('D, d M Y H:i:s') . ")\n\n";
+        echo "Deleting request ID " . $request->getId() . " (creation date: " . $creationDate->format('D, d M Y H:i:s') . ")\n\n";
+        $em->remove($request);
     }
-
-    $em->persist($request);
 }
 
 $em->flush();
