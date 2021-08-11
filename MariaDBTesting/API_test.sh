364aabbc86e4f3b4376bd5945c9aca10d07ac316
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2021-08-11 15:38:19 +0000

    Track success/failure and rename variables

--- MariaDBTesting/API_test.sh
+++ MariaDBTesting/API_test.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-oracle_dir=/usr/share/GOCDB5/MariaDBTesting/originalDB
-maria_dir=/usr/share/GOCDB5/MariaDBTesting/newDB
-diff_dir=/usr/share/GOCDB5/MariaDBTesting/diffDB
+# Directories where XML will be saved
+oracle_dir=/usr/share/GOCDB5/MariaDBTesting/oracle
+mariadb_dir=/usr/share/GOCDB5/MariaDBTesting/mariadb
 
+# Directory for diffs of XML files
+diff_dir=/usr/share/GOCDB5/MariaDBTesting/xmlDiff
+
+# Base URLs for databases
 oracle_URL="https://goc.egi.eu"
-maria_URL="https://host-172-16-102-162.nubes.stfc.ac.uk"
+mariadb_URL="https://host-172-16-102-162.nubes.stfc.ac.uk"
 
 grid_dir="/etc/grid-security/"
 cert_dir="${grid_dir}hostcert/"
 
 # Declare a string array
-arrMethods=()
-arrFiles=()
-arrPermissions=()
+arr_methods=()
+arr_files=()
+arr_permissions=()
+
+#
+diff_count=0
+arr_file_failures=()
 
 while test $# -gt 0; do
     case "$1" in
@@ -49,129 +57,129 @@ while test $# -gt 0; do
             ;;
         --all)
             shift
-            arrMethods+=("site" "site_list" "site_contacts" "site_security_info" "roc_list" "roc_contacts" "downtime&startdate=2021-01-01" "downtime_nested_services&startdate=2021-01-01" "service_endpoint" "service" "service_types" "user" "downtime_to_broadcast" "cert_status_changes" "cert_status_date" "service_group" "service_group_role" "ngi" "project_contacts" "site_count_per_country")
-            arrFiles+=("sites" "sites_list" "site_contacts" "site_security_info" "roc_list" "roc_contacts" "downtimes" "downtime_services" "service_endpoints" "services" "service_types" "users" "downtimes_to_broadcast" "cert_status_changes" "cert_status_dates" "service_groups" "service_group_roles" "ngis" "project_contacts" "site_counts")
-            arrPermissions+=(2 1 2 2 1 2 1 1 1 1 1 2 1 2 2 2 2 2 2 1)
+            arr_methods+=("site" "site_list" "site_contacts" "site_security_info" "roc_list" "roc_contacts" "downtime&startdate=2021-01-01" "downtime_nested_services&startdate=2021-01-01" "service_endpoint" "service" "service_types" "user" "downtime_to_broadcast" "cert_status_changes" "cert_status_date" "service_group" "service_group_role" "ngi" "project_contacts" "site_count_per_country")
+            arr_files+=("sites" "sites_list" "site_contacts" "site_security_info" "roc_list" "roc_contacts" "downtimes" "downtime_services" "service_endpoints" "services" "service_types" "users" "downtimes_to_broadcast" "cert_status_changes" "cert_status_dates" "service_groups" "service_group_roles" "ngis" "project_contacts" "site_counts")
+            arr_permissions+=(2 1 2 2 1 2 1 1 1 1 1 2 1 2 2 2 2 2 2 1)
             ;;
         --sites)
             shift
-            arrMethods+=("site")
-            arrFiles+=("sites")
-            arrPermissions+=(2)
+            arr_methods+=("site")
+            arr_files+=("sites")
+            arr_permissions+=(2)
             ;;
         --sites_list)
             shift
-            arrMethods+=("site_list")
-            arrFiles+=("sites_list")
-            arrPermissions+=(1)
+            arr_methods+=("site_list")
+            arr_files+=("sites_list")
+            arr_permissions+=(1)
             ;;
         --site_contacts)
             shift
-            arrMethods+=("site_contacts")
-            arrFiles+=("site_contacts")
-            arrPermissions+=(2)
+            arr_methods+=("site_contacts")
+            arr_files+=("site_contacts")
+            arr_permissions+=(2)
             ;;
         --site_security_info)
             shift
-            arrMethods+=("site_security_info")
-            arrFiles+=("site_security_info")
-            arrPermissions+=(2)
+            arr_methods+=("site_security_info")
+            arr_files+=("site_security_info")
+            arr_permissions+=(2)
             ;;
         --roc_list)
             shift
-            arrMethods+=("roc_list")
-            arrFiles+=("roc_list")
-            arrPermissions+=(1)
+            arr_methods+=("roc_list")
+            arr_files+=("roc_list")
+            arr_permissions+=(1)
             ;;
         --roc_contacts)
             shift
-            arrMethods+=("roc_contacts")
-            arrFiles+=("roc_contacts")
-            arrPermissions+=(2)
+            arr_methods+=("roc_contacts")
+            arr_files+=("roc_contacts")
+            arr_permissions+=(2)
             ;;
         --downtimes)
             shift
-            arrMethods+=("downtime&startdate=2021-01-01")
-            arrFiles+=("downtimes")
-            arrPermissions+=(1)
+            arr_methods+=("downtime&startdate=2021-01-01")
+            arr_files+=("downtimes")
+            arr_permissions+=(1)
             ;;
         --downtime_services)
             shift
-            arrMethods+=("downtime_nested_services&startdate=2021-01-01")
-            arrFiles+=("downtime_services")
-            arrPermissions+=(1)
+            arr_methods+=("downtime_nested_services&startdate=2021-01-01")
+            arr_files+=("downtime_services")
+            arr_permissions+=(1)
             ;;
         --service_endpoints)
             shift
-            arrMethods+=("service_endpoint")
-            arrFiles+=("service_endpoints")
-            arrPermissions+=(1)
+            arr_methods+=("service_endpoint")
+            arr_files+=("service_endpoints")
+            arr_permissions+=(1)
             ;;
         --services)
             shift
-            arrMethods+=("service")
-            arrFiles+=("services")
-            arrPermissions+=(1)
+            arr_methods+=("service")
+            arr_files+=("services")
+            arr_permissions+=(1)
             ;;
         --service_types)
             shift
-            arrMethods+=("service_types")
-            arrFiles+=("service_types")
-            arrPermissions+=(1)
+            arr_methods+=("service_types")
+            arr_files+=("service_types")
+            arr_permissions+=(1)
             ;;
         --users)
             shift
-            arrMethods+=("user")
-            arrFiles+=("users")
-            arrPermissions+=(2)
+            arr_methods+=("user")
+            arr_files+=("users")
+            arr_permissions+=(2)
             ;;
         --downtimes_to_broadcast)
             shift
-            arrMethods+=("downtime_to_broadcast")
-            arrFiles+=("downtimes_to_broadcast")
-            arrPermissions+=(1)
+            arr_methods+=("downtime_to_broadcast")
+            arr_files+=("downtimes_to_broadcast")
+            arr_permissions+=(1)
             ;;
         --cert_status_changes)
             shift
-            arrMethods+=("cert_status_changes")
-            arrFiles+=("cert_status_changes")
-            arrPermissions+=(2)
+            arr_methods+=("cert_status_changes")
+            arr_files+=("cert_status_changes")
+            arr_permissions+=(2)
             ;;
         --cert_status_dates)
             shift
-            arrMethods+=("cert_status_date")
-            arrFiles+=("cert_status_dates")
-            arrPermissions+=(2)
+            arr_methods+=("cert_status_date")
+            arr_files+=("cert_status_dates")
+            arr_permissions+=(2)
             ;;
         --service_groups)
             shift
-            arrMethods+=("service_group")
-            arrFiles+=("service_groups")
-            arrPermissions+=(2)
+            arr_methods+=("service_group")
+            arr_files+=("service_groups")
+            arr_permissions+=(2)
             ;;
         --service_group_roles)
             shift
-            arrMethods+=("service_group_role")
-            arrFiles+=("service_group_roles")
-            arrPermissions+=(2)
+            arr_methods+=("service_group_role")
+            arr_files+=("service_group_roles")
+            arr_permissions+=(2)
             ;;
         --ngis)
             shift
-            arrMethods+=("ngi")
-            arrFiles+=("ngis")
-            arrPermissions+=(2)
+            arr_methods+=("ngi")
+            arr_files+=("ngis")
+            arr_permissions+=(2)
             ;;
         --project_contacts)
             shift
-            arrMethods+=("project_contacts")
-            arrFiles+=("project_contacts")
-            arrPermissions+=(2)
+            arr_methods+=("project_contacts")
+            arr_files+=("project_contacts")
+            arr_permissions+=(2)
             ;;
         --site_counts)
             shift
-            arrMethods+=("site_count_per_country")
-            arrFiles+=("site_counts")
-            arrPermissions+=(1)
+            arr_methods+=("site_count_per_country")
+            arr_files+=("site_counts")
+            arr_permissions+=(1)
             ;;
         *)
             break
@@ -184,33 +192,59 @@ searchString="<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 # Get all required data from API
 
 # Iterate the loop to read and print each array element
-for i in "${!arrMethods[@]}"
+for i in "${!arr_methods[@]}"
 do
-    if [[ ${arrPermissions[i]} = 2 ]]; then
+    if [[ ${arr_permissions[i]} = 2 ]]; then
         wgetOptionsOracle="--ca-certificate=${grid_dir}hostcert.pem  --certificate=/${cert_dir}hostcert.pem --private-key=${cert_dir}hostkey.pem --private-key-type=PEM"
         wgetOptionsMaria=$wgetOptionsOracle
-        public="private"
+        privacy="private"
     else
         wgetOptionsOracle="--no-check-certificate"
         wgetOptionsMaria=""
-        public="public"
+        privacy="public"
     fi
 
-    wget $wgetOptionsOracle -O $oracle_dir/${arrFiles[i]}.xml "${oracle_URL}/gocdbpi/${public}/?method=get_${arrMethods[i]}"
-    wget --no-check-certificate $wgetOptionsMaria -O $maria_dir/${arrFiles[i]}.xml "${maria_URL}/gocdbpi/${public}/?method=get_${arrMethods[i]}"
+    oracle_file=$oracle_dir/${arr_files[i]}.xml
+    mariadb_file=$mariadb_dir/${arr_files[i]}.xml
+    diff_file=$diff_dir/${arr_files[i]}.txt
 
-    if grep -q "$searchString"  "$oracle_dir/${arrFiles[i]}.xml"; then
-        echo ${arrFiles[i]}.xml downloaded successfully
+    success=true
+
+    wget $wgetOptionsOracle -O $oracle_file "${oracle_URL}/gocdbpi/${privacy}/?method=get_${arr_methods[i]}"
+    wget --no-check-certificate $wgetOptionsMaria -O $mariadb_file "${mariadb_URL}/gocdbpi/${privacy}/?method=get_${arr_methods[i]}"
+
+    if grep -q "$searchString"  "$oracle_file"; then
+        echo $oracle_file downloaded successfully!
     else
-        echo ${arrFiles[i]}.xml download failed
+        echo $oracle_file download failed
+        success=false
+        arr_file_failures+=($oracle_file)
     fi
 
-    if grep -q "$searchString"  "$maria_dir/${arrFiles[i]}.xml"; then
-        echo ${arrFiles[i]}.xml downloaded successfully
+    if grep -q "$searchString"  "$mariadb_dir/${arr_files[i]}.xml"; then
+        echo $mariadb_file downloaded successfully!
     else
-        echo ${arrFiles[i]}.xml download failed
+        echo $mariadb_file download failed
+        success=false
+        arr_file_failures+=($mariadb_file)
     fi
 
-    diff $oracle_dir/${arrFiles[i]}.xml $maria_dir/${arrFiles[i]}.xml > $diff_dir/${arrFiles[i]}.txt
+    if $success; then
+        echo performing diff pf XML files...
+        diff $oracle_file $mariadb_file > $diff_file
+        diff_count=$((diff_count+1))
+    fi
 
 done
+
+echo diffs created successfully: $diff_count
+
+if [[ ${#arr_file_failures[@]} = 0 ]]; then
+    echo All XML downloads successful
+else
+    echo XML download failures:
+    for i in "${!arr_file_failures[@]}"
+    do
+        echo ${arr_file_failures[i]}
+    done
+fi
--- MariaDBTesting/line_count.sh
+++ MariaDBTesting/line_count.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-ls diffDB > diffs.txt
+ls xmlDiff > diffs.txt
 length=`wc -l < diffs.txt`
 
 for (( i = 1; i <= $length; i++ ))
 do
     diff_file=`head -$i diffs.txt | tail -1`
-    diff_lines=`wc -l < diffDB/$diff_file`
+    diff_lines=`wc -l < xmlDiff/$diff_file`
 
-    xml_file=`basename diffDB/$diff_file | cut -f 1 -d .`
-    total_lines=`wc -l < originalDB/${xml_file}.xml`
-    #total_lines+=`wc -l < newDB/${xml_file}.xml`
+    xml_file=`basename xmlDiff/$diff_file | cut -f 1 -d .`
+    total_lines=`wc -l < oracle/${xml_file}.xml`
+    #total_lines+=`wc -l < mariadb/${xml_file}.xml`
 
     percent=$((100 * $diff_lines / $total_lines))
     #percent=$((100 * $total_lines))
